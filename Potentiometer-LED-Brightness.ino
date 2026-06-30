5ac1848c474421f3f035586630ff04f543fb6b8e
N-GeethaPallavi <geethapallavi696@gmail.com>
2026-06-30 19:29:35 +0530

    Create Potentiometer-LED-Brightness.ino
    Configured an analog potentiometer as an input to the ESP32 ADC and dynamically controlled an LED's brightness using PWM

---
+++ Potentiometer-LED-Brightness.ino
@@ -0,0 +1,18 @@
+# define LED 13 
+void setup () 
+{ 
+pinMode(LED,OUTPUT); 
+}
+void loop ()  
+{  
+analogWrite(LED,50); 
+delay(1000); 
+analogWrite(LED,100); 
+delay(1000); 
+analogWrite(LED,150); 
+delay(1000); 
+analogWrite(LED,200);  
+delay(1000); 
+analogWrite(LED,250); 
+delay(1000);
+}
